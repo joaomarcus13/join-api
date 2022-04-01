4711bb398b85d1d880c412c07f3c439fdcbdb5c0
joaomarcus13 <joaomarcus13@hotmail.com>
2022-04-01 10:04:23 -0300

    merge connection

--- src/database/request.js
+++ src/database/request.js
@@ -1,14 +1,5 @@
-// import sql from 'mssql';
-// import sqlConfig from '../config/database.js';
 import { logger } from '../util/logger.js';
 import conn from './connection.js';
-
-// const pool = new sql.ConnectionPool(sqlConfig);
-// pool.connect((err) => {
-//   err
-//     ? logger.error(err.message)
-//     : logger.info('Successful database connection');
-// });
 
 export default async function (queryString) {
   try {
@@ -19,15 +10,3 @@ export default async function (queryString) {
     throw new Error(err);
   }
 }
-
-// export default async function db(queryString, database = 'nugei') {
-//   try {
-//     const conn = await new sql.ConnectionPool(sqlConfig(database)).connect();
-//     const result = await conn.request().query(queryString);
-//     conn.close();
-//     return result;
-//   } catch (err) {
-//     logger.error(err.message);
-//     throw new Error(err);
-//   }
-// }
