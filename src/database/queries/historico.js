5f636ca086be4d9a94962c456ec2ff7da2da7dbe
joaomarcus13 <joaomarcus13@hotmail.com>
2022-04-01 11:34:10 -0300

    remove comments

--- src/database/queries/historico.js
+++ src/database/queries/historico.js
@@ -3,10 +3,3 @@ export default {
     `insert into dbo.historico_localizacao (municipio,data_acesso, item_pesquisado) values ('${municipio}', '${data}', '${item}')`,
   select: () => `select * from dbo.historico_localizacao`,
 };
-
-// ({ municipio, data, item }) => {
-//   return {
-//     insert: `insert into dbo.historico_localizacao (municipio,data_acesso, item_pesquisado) values ('${municipio}', '${data}', '${item}')`,
-//     select: `select * from dbo.historico_localizacao`,
-//   };
-// };
